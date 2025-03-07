51f48ed6444d8291106c3b76881f0b690eda8e50
AnonymRo <sebastianbusa@icloud.com>
2025-03-07 15:20:49 +0200

    Update for CupCakeCorner app
    Added accessibility traits for hiding unnecessary image and loading spinner from VoiceOver

--- CupcakeCorner/CupcakeCorner/View/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/View/CheckoutView.swift
@@ -18,8 +18,10 @@ struct CheckoutView: View {
                     image
                         .resizable()
                         .scaledToFit()
+                        .accessibilityHidden(true)
                 } placeholder: {
                     ProgressView()
+                        .accessibilityHidden(true)
                 }
                 .frame(height: 233)
                 
